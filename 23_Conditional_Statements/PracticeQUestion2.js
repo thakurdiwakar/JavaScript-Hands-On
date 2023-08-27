359818a4b05252e164390723084e841b93789aa1
diwakarsingh <ds6228353@gmail.com>
2023-08-27 22:20:44 +0530

    27=08-2023

---
+++ 23_Conditional_Statements/PracticeQUestion2.js
@@ -0,0 +1,20 @@
+// Create a system to calculate popcorn prices based on the size customer asked for :
+
+// if size is 'XL', price is Rs.250
+// if size is 'L', price is Rs.200
+// if size is 'M', price is Rs.100
+// if size is 'S', price is Rs.50
+
+let size = "S";
+
+if (size === "XL") {
+  console.log("Price is Rs.250");
+} else if (size === "L") {
+  console.log("Price is Rs.200");
+} else if (size === "M") {
+  console.log("price is Rs.100");
+} else if (size === "S") {
+  console.log("price is Rs.50");
+} else {
+  console.log("Invalid Size");
+}
---
+++ 23_Conditional_Statements/else_if_statement.js
@@ -0,0 +1,10 @@
+// Create a traffic light system that shows what to do based on color
+
+var color = 'red' ;
+if (color === "red") {
+  console.log("Stop");
+} else if (color === "yellow") {
+  console.log("Slow down and wait");
+} else {
+  console.log("Go");
+}
