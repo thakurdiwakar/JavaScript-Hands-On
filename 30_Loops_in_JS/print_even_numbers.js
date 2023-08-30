9a1ac8ab889cbca8ac46c86dd8916957ef38db67
diwakarsingh <ds6228353@gmail.com>
2023-08-30 20:31:37 +0530

    30-08-2023

---
+++ 30_Loops_in_JS/print_even_numbers.js
@@ -0,0 +1,4 @@
+for (let i = 2; i <= 10; i=i + 2) {
+    console.log("The even numbers between 2 to 10 are :", i);
+  }
+  
---
+++ 30_Loops_in_JS/print_odd-numbers.js
@@ -0,0 +1,3 @@
+for (let i = 1; i <= 15; i=i + 2) {
+  console.log("The odd numbers between 1 to 15 are :", i);
+}
