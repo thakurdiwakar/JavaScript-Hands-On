07b6880cf4066801fca99aa6c83ad309a803aed3
diwakarsingh <ds6228353@gmail.com>
2023-08-31 14:02:29 +0530

    31-08-2023

---
+++ 32_Object_Literals/add_updateValues.js
@@ -0,0 +1,14 @@
+const myInfo = {
+  firstName: "Diwakar",
+  lastName: "Singh",
+  location: "Greater Noida",
+  email: "diwkr14@gmail.com",
+  age: 21,
+  cgpa: 9.45,
+};
+
+myInfo.firstName = "Kush";
+myInfo.gender = "Male";
+
+console.log(myInfo);
+console.log(myInfo.firstName);
