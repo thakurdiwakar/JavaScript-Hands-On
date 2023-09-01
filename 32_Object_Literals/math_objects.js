51150f95e7d422448e3f80f54130ac639605e702
diwakarsingh <ds6228353@gmail.com>
2023-09-01 07:44:46 +0530

    01-09-2023

---
+++ 32_Object_Literals/math_objects.js
@@ -0,0 +1,16 @@
+console.log(Math.PI);
+console.log(typeof Math.PI);
+
+console.log(Math.E);
+
+console.log(Math.pow(2,3));
+
+console.log(Math.sqrt(25));
+
+console.log(Math.abs(-13.45));
+
+console.log(Math.floor(13.45));
+
+console.log(Math.ceil(13.45));
+
+console.log(Math.random());
