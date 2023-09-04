f8c6c3226e3aca80b27b30ca9edb172ca7b77d50
diwakarsingh <ds6228353@gmail.com>
2023-09-04 15:28:21 +0530

    04-09-2023

---
+++ 33_Functions/PracticeQuestion.js
@@ -0,0 +1,7 @@
+function printPoem() {
+  console.log(
+    "Twinkle, twinkle, little star\nHow I wonder what you are\nUp above the world so high\nLike a diamond in the sky\nTwinkle, twinkle little star\nHow I wonder what you are\nWhen the blazing sun is gone\nWhen he nothing shines upon\nThen you show your little light\nTwinkle, twinkle, all the night\nTwinkle, twinkle, little star\nHow I wonder what you are "
+  );
+}
+
+printPoem();
