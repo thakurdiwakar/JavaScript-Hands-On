b44aa1552e2845e876533269f3fe07dcaec3002e
diwakarsingh <ds6228353@gmail.com>
2023-09-04 15:08:36 +0530

    04-09-2023

---
+++ 33_Functions/function.js
@@ -0,0 +1,21 @@
+function hello() {
+  console.log("Hello,Coders");
+}
+
+function printName() {
+  for (let i = 0; i < 10; i++) {
+    console.log("Diwakar Singh");
+  }
+}
+
+function evenOdd() {
+  let n = 17;
+  if (n % 2 == 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+}
+
+hello();
+printName();
