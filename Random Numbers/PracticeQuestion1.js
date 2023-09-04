3661a527954328e60b7f67d1756cb2401137e965
diwakarsingh <ds6228353@gmail.com>
2023-09-04 07:43:09 +0530

    04-09-2023

---
+++ Random Numbers/PracticeQuestion1.js
@@ -0,0 +1,3 @@
+let newRand = Math.floor(Math.random() * 100)+1 ;//generating a random number between 1 to 100
+
+console.log(newRand);
---
+++ Random Numbers/PracticeQuestion2.js
@@ -0,0 +1,3 @@
+let newRand = Math.floor(Math.random() * 5) + 1; //generating a random number between 1 to 5
+
+console.log(newRand);
