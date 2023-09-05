0331e7708c97dd9f319cb9a54f9f97e66f191852
diwakarsingh <ds6228353@gmail.com>
2023-09-05 07:18:22 +0530

    05-09-2023

---
+++ 33_Functions/PracticeQuestion3.js
@@ -0,0 +1,6 @@
+function calculateAverage(num1,num2,num3)
+{
+    console.log((num1+num2+num3)/3);
+}
+
+calculateAverage(2,2,2);
---
+++ 33_Functions/function_with_arguments.js
@@ -0,0 +1,9 @@
+function funWithArgument(firstName, lastName) {
+  console.log(firstName, lastName);
+}
+funWithArgument("Diwakar", "Singh");
+
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+console.log(addTwoNumbers(2, 3));
