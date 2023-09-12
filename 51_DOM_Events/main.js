386e74dbb7214d95bf0552b270939dea706e2a76
diwakarsingh <ds6228353@gmail.com>
2023-09-13 00:53:53 +0530

    12-09-2023

---
+++ 51_DOM_Events/main.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector("button");
+
+// console.log(btn);
+
+// btn.onclick = function () {
+//   console.log("CLicked");
+//   alert("CLicked");
+// };
+
+
+
+
+btn.addEventListener("click",function() {
+    console.log("CLicked");
+});
