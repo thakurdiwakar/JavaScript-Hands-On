a4708dd3eb6f72da9fbe4343e635953ce618f18a
diwakarsingh <ds6228353@gmail.com>
2023-09-12 22:19:01 +0530

    12-09-2023

---
+++ 50_Starter_Code/main.js
@@ -0,0 +1,23 @@
+let para1 = document.createElement("p");
+
+para1.innerText = "hey I'm red";
+
+document.querySelector("body").append(para1);
+
+para1.classList.add("red");
+
+let head1 = document.createElement("h3");
+
+head1.innerText = "I'm a blue h3!";
+
+document.querySelector("body").append(head1);
+
+head1.classList.add("blue");
+
+let div1 = document.createElement("div");
+
+let h1 = "I am in div";
+let p1 = "Me too";
+div1.innerHTML("head1");
+document.querySelector("body").append(div1);
+div1.classList.add("div");
