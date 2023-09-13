279867170d9988d9ffb4052efd31ae86dd9d1871
diwakarsingh <ds6228353@gmail.com>
2023-09-13 22:47:51 +0530

    13-09-2023

--- 54_Form_Events/main.js
+++ 54_Form_Events/main.js
@@ -1,7 +1,14 @@
 let form = document.querySelector("form");
 
-form.addEventListener("submit", function () {
+form.addEventListener("submit", function (event) {
   alert("Form Submitted");
 
   event.preventDefault();
+
+  let input = document.querySelector("input");
+
+  // console.log(input);
+
+  console.log(input.value);
+  // console.log(input.innerText);
 });
