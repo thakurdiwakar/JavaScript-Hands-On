c8558f938023760aa0370d079aaa23a23f927ebe
diwakarsingh <ds6228353@gmail.com>
2023-09-14 00:40:56 +0530

    14-03-2023

--- 54_Form_Events/main.js
+++ 54_Form_Events/main.js
@@ -1,14 +1,33 @@
 let form = document.querySelector("form");
-
+//submit event
 form.addEventListener("submit", function (event) {
-  alert("Form Submitted");
+  // alert("Form Submitted");
 
   event.preventDefault();
 
-  let input = document.querySelector("input");
+  // let input = document.querySelector("input");
 
   // console.log(input);
 
-  console.log(input.value);
+  // console.log(input.value);
   // console.log(input.innerText);
 });
+//change event//
+
+
+// let user = document.querySelector("#user");
+
+// user.addEventListener("change", function (event) {
+//   console.log("Input Chnaged");
+//   console.log("Value", user.value);
+// });
+
+//input event
+
+let user = document.querySelector("#user");
+
+user.addEventListener("input", function (event) {
+  console.log("Input ");
+  console.log("Value", user.value);
+});
+
---
+++ 55_Text_Editor/main.js
@@ -0,0 +1,10 @@
+let inp=document.querySelector("#text");
+
+let p1=document.querySelector("p");
+
+inp.addEventListener("input",function(event)
+{
+    // console.log(inp.value);
+
+    p1.innerText=this.value;
+})
