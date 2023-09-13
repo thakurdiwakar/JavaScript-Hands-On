6301c5e576a5af8847aa9ec7a17735dfe46004cd
diwakarsingh <ds6228353@gmail.com>
2023-09-13 13:57:42 +0530

    13-09-2023

--- 52_Event_Listeners/main.js
+++ 52_Event_Listeners/main.js
@@ -5,10 +5,25 @@
 // });
 let btn = document.querySelector("button");
 
+let h1 = document.querySelector("h1");
+let p1 = document.querySelector("p");
+
 btn.addEventListener("click", function () {
   //   console.log(this); //it will print button
 
   // console.dir(this);
+  console.dir(this.innerText);
+
+  this.style.color = "blue";
+});
+
+h1.addEventListener("click", function () {
+  console.dir(this.innerText);
+
+  this.style.color = "blue";
+});
+
+p1.addEventListener("click", function () {
   console.dir(this.innerText);
 
   this.style.color = "blue";
