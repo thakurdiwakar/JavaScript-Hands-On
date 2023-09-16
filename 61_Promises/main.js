e0246b5b6f35568121ad6b6eeba727a1b46c750c
diwakarsingh <ds6228353@gmail.com>
2023-09-16 20:13:30 +0530

    16-09-2023

--- 61_Promises/main.js
+++ 61_Promises/main.js
@@ -1,10 +1,26 @@
 function saveToDb(data) {
-    return new Promise((resolve, reject) => {
-      let internetSpeed = Math.floor(Math.random() * 10) + 1;
-      if (internetSpeed > 4) {
-        resolve("success: data was saved");
-      } else {
-        reject("failure: weak connection");
-      }
-    });
-  }
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("success: data was saved");
+    } else {
+      reject("failure: weak connection");
+    }
+  });
+}
+
+// then and catch methods
+
+let request = saveToDb("Save DataBase");
+
+// console.log(request);
+
+request
+  .then(() => {
+    console.log("Promise was resolved");
+    console.log(request);
+  })
+  .catch(() => {
+    console.log("Promise was rejected");
+    console.log(request);
+  });
